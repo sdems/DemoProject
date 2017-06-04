7d054ee7a746e9f39d89263fa9f2fdc12701ba94
unknown <samir.dems@outlook.com>
2017-06-04 17:59:32 +0200

    merging commit

--- PlugWine.Console/Program.cs
+++ PlugWine.Console/Program.cs
@@ -5,6 +5,7 @@ namespace PlugWine.Console
 {
     // Change from remote without conflicts
 	// Change from local without conflicts
+// Change from local with conflicts
     class Program
     {
         private static Service _service;
